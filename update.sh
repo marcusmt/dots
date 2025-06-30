4b22fd41add2f781d1f158c08f0658d2d8b5fe4b
Marcus Sousa <sousa.mmta@gmail.com>
2025-06-30 23:56:45 +0200

    Niri Environment Default Config

--- update.sh
+++ update.sh
@@ -6,6 +6,16 @@ unzip $HOME/Downloads/Hack.zip -d $HOME/.fonts
 fc-cache -fv
 rm $HOME/Downloads/Hack.zip
 
+#FZF
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --all
+
+#Neovim and its dependencies
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+rm -rf nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim-linux-x86_64
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+
 # Dots
 cp -r niri ~/.config/
 cp -r swaync ~/.config/
