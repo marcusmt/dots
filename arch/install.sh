039d0b2ce1de351368b3b26e1b14dad8b30ea239
Marcus Sousa <sousa.mmta@gmail.com>
2025-08-03 23:10:40 +0200

    testing

--- arch/install.sh
+++ arch/install.sh
@@ -1,19 +1,14 @@
 #!/bin/sh
 
 packages=(
-  "hyprland"
-  "wofi"
   "bluez"
   "bluez-utils"
-  "qt6ct"
   "gnome-themes-extra"
   "nwg-look"
   "ttf-hack-nerd"
   "brightnessctl"
-  "hyprlock"
-  "waybar"
-  "swaync"
-  "eza"
+  "network-manager-applet"
+  "nm-connection-editor"
 )
 
 curl -sS https://starship.rs/install.sh | sh -s -- -y
@@ -21,10 +16,5 @@ curl -sS https://starship.rs/install.sh | sh -s -- -y
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 ./1password.sh
-./paru.sh
 
 paru -S "${packages[@]}"
-
-cp -r ../hypr $HOME/.config
-cp -r ../swaync $HOME/.config
-cp -r ../waybar $HOME/.config
