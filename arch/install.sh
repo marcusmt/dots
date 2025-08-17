509219ece9e63e3760ca14474778e79fa8cadbe4
Marcus Sousa <sousa.mmta@gmail.com>
2025-08-18 00:56:28 +0200

    arch update

--- arch/install.sh
+++ arch/install.sh
@@ -1,48 +1,24 @@
-#!/bin/sh
+#!/usr/bin/env fish
 
-packages-x11=(
-  "nwg-look"
-  "i3"
-  "plasma-x11-session"
-  "dunst"
-  "picom"
-  "feh"
-)
+# Define os arrays de pacotes
+set packages_x11 nwg-look i3 plasma-x11-session dunst picom feh
+set packages_wayland hypridle hyprlock wl-clip-persist grim slurp xwaylandvideobridge-git xdg-desktop-portal-hyprland swaybg swaync waybar swayosd-git qt5-wayland qt6-wayland wofi hyprshot wlogout
+set packages_common qt6ct gnome-themes-extra ttf-hack-nerd brightnessctl eza ghostty-git
 
-packages-wayland=(
-  "hypridle"
-  "hyprlock"
-  "wl-clip-persist"
-  "grim"
-  "slurp"
-  "xwaylandvideobridge-git"
-  "xdg-desktop-portal-hyprland"
-  "swaybg"
-  "swaync"
-  "waybar"
-  "swayosd-git"
-  "qt5-wayland"
-  "qt6-wayland"
-  
-)
-
-packages-common=(
-  "qt6ct"
-  "gnome-themes-extra"
-  "ttf-hack-nerd"
-  "brightnessctl"
-  "eza"
-)
-
+# Instala o Starship
 curl -sS https://starship.rs/install.sh | sh -s -- -y
 
+# Instala o Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-./1password.sh
-./paru.sh
+# Instala os pacotes com paru
+paru -S $packages_wayland $packages_common --noconfirm
 
-paru -S "${packages[@]}"
-
+# Copia os arquivos de configuração
 cp -r ../hypr $HOME/.config
 cp -r ../swaync $HOME/.config
 cp -r ../waybar $HOME/.config
+cp -r ../wofi $HOME/.config
+cp -r ../fish $HOME/.config
+
+echo "Instalação concluída!"
