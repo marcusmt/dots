0cde5827191c66c6dd9b22f3178fa809a3c8b498
Marcus Sousa <sousa.mmta@gmail.com>
2025-08-26 14:03:08 +0200

    hypr arch setup

--- arch/install.sh
+++ arch/install.sh
@@ -1,24 +1,58 @@
-#!/usr/bin/env fish
+#!/usr/bin/env bash
 
-# Define os arrays de pacotes
-set packages_x11 nwg-look i3 plasma-x11-session dunst picom feh
-set packages_wayland hypridle hyprlock wl-clip-persist grim slurp xwaylandvideobridge-git xdg-desktop-portal-hyprland swaybg swaync waybar swayosd-git qt5-wayland qt6-wayland wofi hyprshot wlogout
-set packages_common qt6ct gnome-themes-extra ttf-hack-nerd brightnessctl eza ghostty-git
+sudo pacman -Syu
+sudo pacman -S git
 
-# Instala o Starship
+# Define a single, sorted array of all packages
+declare -a packages=(
+  brightnessctl
+  eza
+  ghostty-git
+  google-chrome
+  gnome-themes-extra
+  grim
+  hypridle
+  hyprland
+  hyprlock
+  hyprshot
+  neovim
+  qt5-wayland
+  qt6-wayland
+  qt6ct
+  slurp
+  swaybg
+  swaync
+  swayosd-git
+  ttf-hack-nerd
+  waybar
+  wl-clip-persist
+  wlogout
+  wofi
+  xdg-desktop-portal-hyprland
+)
+
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
+
+# Install Starship
 curl -sS https://starship.rs/install.sh | sh -s -- -y
 
-# Instala o Rust
+# Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-# Instala os pacotes com paru
-paru -S $packages_wayland $packages_common --noconfirm
+# TODO add paru and 1password build
 
-# Copia os arquivos de configuração
+# Install packages with paru
+paru -S "${packages[@]}" --noconfirm
+
+systemctl start swayosd-libinput-backend.service
+
+# Copy configuration files
 cp -r ../hypr $HOME/.config
 cp -r ../swaync $HOME/.config
 cp -r ../waybar $HOME/.config
 cp -r ../wofi $HOME/.config
 cp -r ../fish $HOME/.config
+sudo cp xdg-desktop-portal-hyprland /usr/share/xdg-desktop-portal/
 
 echo "Instalação concluída!"
