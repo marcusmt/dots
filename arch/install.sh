99c7c918b7568445f3ea6fbcf42aee70e59bb38b
Marcus Sousa <sousa.mmta@gmail.com>
2025-08-28 22:18:26 +0200

    testing new config

--- arch/install.sh
+++ arch/install.sh
@@ -5,54 +5,26 @@ sudo pacman -S git
 
 # Define a single, sorted array of all packages
 declare -a packages=(
-  brightnessctl
-  eza
-  ghostty-git
-  google-chrome
-  gnome-themes-extra
-  grim
-  hypridle
-  hyprland
-  hyprlock
-  hyprshot
-  neovim
-  qt5-wayland
-  qt6-wayland
-  qt6ct
-  slurp
-  swaybg
-  swaync
-  swayosd-git
-  ttf-hack-nerd
-  waybar
-  wl-clip-persist
-  wlogout
-  wofi
-  xdg-desktop-portal-hyprland
+  ghostty
+  fish
 )
-
-git clone https://github.com/LazyVim/starter ~/.config/nvim
-rm -rf ~/.config/nvim/.git
 
 # Install Starship
 curl -sS https://starship.rs/install.sh | sh -s -- -y
 
-# Install Rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
-# TODO add paru and 1password build
-
 # Install packages with paru
-paru -S "${packages[@]}" --noconfirm
+paru -S --needed --noconfirm "${packages[@]}"
 
-systemctl start swayosd-libinput-backend.service
+# Brave
+curl -fsS https://dl.brave.com/install.sh | sh
 
-# Copy configuration files
-cp -r ../hypr $HOME/.config
-cp -r ../swaync $HOME/.config
-cp -r ../waybar $HOME/.config
-cp -r ../wofi $HOME/.config
-cp -r ../fish $HOME/.config
-sudo cp xdg-desktop-portal-hyprland /usr/share/xdg-desktop-portal/
+# TODO add paru, 1password, nvidia
+./paru.sh
+./1password.sh
+./nvidia.sh
 
+cp -r ../ghostty $HOME/.config/
+cp -r ../fish $HOME/.config/
+
+./cleanup.sh
 echo "Instalação concluída!"
--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
 # Format man pages
-set -x MANROFFOPT "-c"
+set -x MANROFFOPT -c
 set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
 
 # Useful aliases
@@ -9,5 +9,5 @@ alias la='eza -a --color=always --group-directories-first --icons'
 alias ll='eza -l --color=always --group-directories-first --icons'
 alias lt='eza -aT --color=always --group-directories-first --icons'
 alias l.="eza -a | grep -e '^\.'"
-
+alias cat="bat"
 starship init fish | source
